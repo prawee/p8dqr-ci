a8445c2c394c1e51304fdd16684974f2f7b43422
Prawee Wongsa <prawee@hotmail.com>
2025-03-27 21:35:43 +0700

    update pg connect

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { greet } from './libs/greet';
+export { default as db } from './libs/pg';
--- src/libs/pg.ts
+++ src/libs/pg.ts
@@ -1,9 +1,14 @@
+
 import pg from "pg";
 
-const pool = new pg.Pool({
-    host: "localhost",
-    user: "postgres",
-    database: "postgres",
-    password: "postgres",
-    port: 5432,
-});
+export function dbConnect(props: { host: any; port: any; user: any; database: any; password: any; }) {
+    const pool = new pg.Pool({
+        host: props.host || "localhost",
+        port: props.port || 5432,
+        user: props.user || "postgres",
+        database: props.database || "postgres",
+        password: props.password || "postgres",
+    });
+
+    return pool;
+}
