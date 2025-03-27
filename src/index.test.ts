60bb1e69767f3d05cd60f10847777a6005bc6f4e
Prawee Wongsa <prawee@hotmail.com>
2025-03-27 20:29:59 +0700

    create test code

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import { greet } from "./index";
+
+test("greet function", () => {
+    const name = "World";
+    const expected = `Hello, ${name}!`;
+    const result = greet(name);
+    expect(result).toBe(expected);
+});
